d57719f6c203b11c10e7f1ef9559eda96487a176
Sriya Nukala <sriyasetty13@gmail.com>
2024-01-20 23:00:15 +0530

    Complete setup, about, contact routes, creating route

--- index.js
+++ index.js
@@ -10,21 +10,29 @@ app.use(bodyParser.urlencoded({ extended: true }));
 // app.use(express.static("public"));
 
 app.get("/",(req,res)=>{
-    console.log("Opening.....");
     res.render("index.ejs");
 });
 
-// app.get("/about",(req,res)=>{
-//     res.render("about.ejs");
-// });
+app.get("/about",(req,res)=>{
+    res.render("about.ejs");
+});
 
-// app.get("/contact",(req,res)=>{
-//     res.render("contact.ejs");
-// });
+app.get("/contact",(req,res)=>{
+    res.render("contact.ejs");
+});
 
-// app.post("/submit",(req,res)=>{
+app.post("/submit",(req,res)=>{
+    var title=req.body.title;
+    var author=req.body.author;
+    var bcontent=req.body.blcontent;
+    const freshblog={
+        blogtitle: title,
+        blogauthor: author,
+        blogcontent: bcontent
+    };
+    res.render("index.ejs",{result:freshblog});
 
-// });
+});
 
 app.listen(port,()=>{
     console.log(`Listening to the port ${port}`);
